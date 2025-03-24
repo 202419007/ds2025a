0d2f665f27df19c0c76faa3e3a6772778dffc495
202419007 <202419007@itc.ac.kr>
2025-03-24 17:58:12 +0900

    v0.9 array

--- week03.py
+++ week03.py
@@ -1,5 +1,21 @@
+def duplicate_city(cities):
+    result_city = []
+    s = set()
+
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2:
+            result_city.append(city)
+    return result_city
+
 cities = ['Incheon', 'Seoul', 'Incheon', 'Incheon', 'Gwangju']
-cities = set(cities) #dict, set은 순서 상관X
-cities.add('Incheon')
-cities.add('Suwon')
+# cities = set(cities) #dict, set은 순서 상관X
+# cities.add('Incheon')
+# cities.add('Suwon')
+cities.append('Incheon')
+cities.append('Suwon')
+cities.append('Seoul')
 print(cities)
+print(set(duplicate_city(cities)))
