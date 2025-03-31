47f3f0bd97dda6b29f16380b57e9b6cbf06fc61a
202419007 <202419007@itc.ac.kr>
2025-03-31 17:54:59 +0900

    v1.6 linked list

--- week04.py
+++ week04.py
@@ -1,8 +1,11 @@
+import random
+
+
+
 class Node:
     def __init__(self, data, linke=None):
         self.data = data
         self.link = linke
-
 
 class LinkedList:
     def __init__(self):
@@ -38,11 +41,16 @@ class LinkedList:
         return result + "END"
 
 
-
 ll = LinkedList()
-ll.append(8)
-ll.append(10)
-ll.append(-9)
+for _ in range(10):
+    ll.append(random.randint(1, 20))
 print(ll)
-print(ll.search(99))
 print(ll.search(10))
+
+# ll = LinkedList()
+# ll.append(8)
+# ll.append(10)
+# ll.append(-9)
+# print(ll)
+# print(ll.search(99))
+# print(ll.search(10))
