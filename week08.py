fefa3fb6f7e7d6907d4b4c2da4e2f0608533ad49
202419007 <202419007@itc.ac.kr>
2025-04-28 17:14:22 +0900

    v3.2 tree

--- week08.py
+++ week08.py
@@ -4,6 +4,30 @@ class TreeNode:
 		self.data = None
 		self.right = None
 
+def pre_order(node):  # 전위
+    if node is None:
+        return
+    print(node.data, end="->")
+    pre_order(node.left)
+    pre_order(node.right)
+
+
+def in_order(node):  # 중위
+    if node is None:
+        return
+    in_order(node.left)
+    print(node.data, end="->")
+    in_order(node.right)
+
+
+def post_order(node):  # 후위, node1
+    if node is None:  # 지나감
+        return
+    post_order(node.left)  # 7~12 복제, node2(l) -> node4(l) -> None
+    post_order(node.right)  # node4(r)
+    print(node.data, end="->")  # hw(node4) -> node2(r)...반복
+
+
 node1 = TreeNode()
 node1.data = 'hs'
 
@@ -27,5 +51,11 @@ node6 = TreeNode()
 node6.data = 'sm'
 node3.left = node6
 
-print(node6.data)
-print(node1.right.left.data)
+# print(node6.data)
+# print(node1.right.left.data)
+
+pre_order(node1)
+print()
+in_order(node1)
+print()
+post_order(node1)
